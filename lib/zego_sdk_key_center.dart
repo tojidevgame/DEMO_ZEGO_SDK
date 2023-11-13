d07f4dee8227a076e372a19dd0fed85897f3df7e
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-11-13 22:25:26 +0700

    re init project

---
+++ lib/zego_sdk_key_center.dart
@@ -0,0 +1,5 @@
+class SDKKeyCenter {
+  static const int appID = 881575211;
+  static const String serverSecret = "81e1381aaa6e86a3df8e959856ce6c1e";
+  static const String appSign = "3f769c7fdaddd9a97c718fa6ee1b362d336073b0d051450a12e943e0f2d0ca11";
+}
