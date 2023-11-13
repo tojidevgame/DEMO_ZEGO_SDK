71a3b8f742dae17a0b67d9415028ff575de16b63
tojidevgame <toainguyenprogramminggame@gmail.com>
2023-11-13 22:15:09 +0700

    init project

---
+++ lib/key_center.dart
@@ -0,0 +1,11 @@
+// Get your AppID, AppSign, and serverSecret from ZEGOCLOUD Console
+// [My Projects -> AppID] : https://console.zegocloud.com/project
+const appID = 881575211;
+const appSign = "3f769c7fdaddd9a97c718fa6ee1b362d336073b0d051450a12e943e0f2d0ca11";
+const serverSecret = "81e1381aaa6e86a3df8e959856ce6c1e";
+
+/// The serverSecret is only required when you need to use this demo to build a Flutter web platform.
+/// The web platform requires token authentication due to security issues. To enable you to quickly experience it,
+/// this demo uses client-side code to generate tokens for authentication, which requires the use of serverSecret.
+/// In a real project, you need to generate tokens on the server side and distribute them to the client,
+/// so as to effectively protect the security of your app.
